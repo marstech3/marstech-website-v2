9f907a8ea05d96c07e56dcfb92d99ee421e2c766
Rishabh <rishabh@opinverse.com>
2023-05-02 09:38:46 +0530

    removed Services and Ovations pages and fixed scroll behaviour after removal

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -5,10 +5,10 @@ import { Howl } from 'howler';
 import beep from '../Assets/sounds/beep.mp3'
 import wosh from '../Assets/sounds/wosh2.wav'
 import Home from "./Home/Home";
-import Services from "./Services-page/Services";
+// import Services from "./Services-page/Services";
 import Products from "./Products-page/Products";
 import Buzz from "./Buzz-page/Buzz";
-import Ovations from "./Ovations-page/Ovations";
+// import Ovations from "./Ovations-page/Ovations";
 import About from "./About-page/About";
 import Contact from "./Contact-page/Contact";
 
@@ -43,20 +43,20 @@ function Landing() {
           
           if (y < window.scrollY && qty < sectionQty) {
             console.log("qty before if",qty)
-            if(qty<3 || qty>4){
+            if(qty<2 || qty>3){
               console.log("qty inside if",qty)
               document.querySelector(`section.s${subqty}`).style.transform = "translateY(-100vh)";
               document.querySelector(`section.s${subqty + 1}`).style.transform = "translateY(0)";
             }
             setTimeout(() => {
               setQty(qty + 1);
-              if(qty<3 || qty>4){
+              if(qty<2 || qty>3){
                 setSubQty(subqty + 1)
                 console.log(subqty)
               }
             },100);
           } else if (y > window.scrollY && qty > 1) {
-            if(qty<=3 || qty>5){
+            if(qty<=2 || qty>4){
               console.log("qty in elseif",qty)
               console.log("subqty in elseif",subqty)
               document.querySelector(`section.s${subqty - 1}`).style.transform = "translateY(0)";
@@ -64,7 +64,7 @@ function Landing() {
             }
             setTimeout(() => {
               setQty(qty - 1);
-              if(qty<4 || qty>5){
+              if(qty<3 || qty>4){
                 setSubQty(subqty - 1)
                 console.log(subqty)
               }
@@ -118,13 +118,13 @@ function Landing() {
   return (
     <div>
       <ul className="top-nav hide">
-        <li className={subqty===2 ? "current" : ""}><button onClick={() => {handleNav(2); setQty(2)}} onMouseOver={handlebeep} >S E R V I C E S</button></li>
-        <li className={subqty===3 ? "current" : ""}><button onClick={() => {handleNav(3); setQty(3)}} onMouseOver={handlebeep}>P R O D U C T S</button></li>
-        <li className={subqty===4 ? "current" : ""}><button onClick={() => {handleNav(4); setQty(6)}} onMouseOver={handlebeep}>B U Z Z</button></li>
+        <li className={subqty===2 ? "current" : ""}><button onClick={() => {handleNav(2); setQty(2)}} onMouseOver={handlebeep} >P R O D U C T S</button></li>
+        <li className={subqty===3 ? "current" : ""}><button onClick={() => {handleNav(3); setQty(3)}} onMouseOver={handlebeep}>B U Z Z</button></li>
         <li><div onClick={() => {handleNav(1); setQty(1)}}><img className="logo" src={logo} alt="Marstech logo"></img></div></li>
-        <li className={subqty===5 ? "current" : ""}><button onClick={() => {handleNav(5); setQty(7)}} onMouseOver={handlebeep}>O V A T I O N S</button></li>
-        <li className={subqty===6 ? "current" : ""}><button onClick={() => {handleNav(6); setQty(8)}} onMouseOver={handlebeep}>A B O U T&nbsp;&nbsp;&nbsp;U S</button></li>
-        <li className={subqty===7 ? "current" : ""}><button onClick={() => {handleNav(7); setQty(9)}} onMouseOver={handlebeep}>C O N T A C T</button></li>
+        <li className={subqty===4 ? "current" : ""}><button onClick={() => {handleNav(4); setQty(6)}} onMouseOver={handlebeep}>A B O U T&nbsp;&nbsp;&nbsp;U S</button></li>
+        <li className={subqty===5 ? "current" : ""}><button onClick={() => {handleNav(5); setQty(7)}} onMouseOver={handlebeep}>C O N T A C T</button></li>
+        {/* <li className={subqty===6 ? "current" : ""}><button onClick={() => {handleNav(6); setQty(8)}} onMouseOver={handlebeep}></button></li>
+        <li className={subqty===7 ? "current" : ""}><button onClick={() => {handleNav(7); setQty(9)}} onMouseOver={handlebeep}></button></li> */}
       </ul>
 
 
@@ -134,24 +134,24 @@ function Landing() {
         <li className={subqty===3 ? "acolor" : "pcolor"}>03</li>
         <li className={subqty===4 ? "acolor" : "pcolor"}>04</li>
         <li className={subqty===5 ? "acolor" : "pcolor"}>05</li>
-        <li className={subqty===6 ? "acolor" : "pcolor"}>06</li>
-        <li className={subqty===7 ? "acolor" : "pcolor"}>07</li>
+        {/* <li className={subqty===6 ? "acolor" : "pcolor"}>06</li>
+        <li className={subqty===7 ? "acolor" : "pcolor"}>07</li> */}
       </ul>
 
       <div className="layout">
         <section className="s1"><Home /></section>
 
-        <section className="s2"><Services /></section>
+        <section className="s2"><Products qty={qty}/></section>
 
-        <section className="s3"><Products qty={qty}/></section>
+        <section className="s3"><Buzz /></section>
 
-        <section className="s4"><Buzz /></section>
+        <section className="s4"><About /></section>
 
-        <section className="s5"><Ovations /></section>
+        <section className="s5"><Contact /></section>
 
-        <section className="s6"><About /></section>
+        {/* <section className="s6"><Services /></section>
 
-        <section className="s7"><Contact /></section>
+        <section className="s7"><Ovations /></section> */}
 
         <section></section>   {/* DO NOT REMOVE THIS SECTION */}
         <section></section>   {/* DO NOT REMOVE THIS SECTION */}
